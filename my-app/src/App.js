2144682cb4f815cd83fa67a29c71499c1f39d284
Jason O'Higgins <jason.ohiggins@gmail.com>
2018-07-30 02:03:11 -0500

    App additions
    Column, Row and Container added to App.js file.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,14 @@ import Wrapper from "./components/Wrapper";
 import './App.css';
 import cars from "./cars.json";
 
+function shufflecars(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 class App extends Component {
   state = {
     cars,
@@ -29,8 +37,27 @@ render () {
         <Title>
           Click each car, but not more than once, or you're OUTTA THERE!
         </Title>
+
+        <Container>
+          <Row>
+          {this.state.cars.map(car => (
+              <Column size="md-3 sm-6">
+                <CarsCard
+                  key={car.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={car.id}
+                  image={car.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
         </Wrapper>
-  )
-};
+    );
+  }
+}
 
 export default App;
