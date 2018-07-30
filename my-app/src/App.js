c087144173826784b9983c39e5f88d1067fc453a
Jason O'Higgins <jason.ohiggins@gmail.com>
2018-07-30 02:23:52 -0500

    added event handlers
    added Click, Increase, Reset and Shuffle event handlers to App.js for when pictures are clicked.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import CarsCard from "./components/CarsCard";
+import CarsCard from "./my-app/components/CarsCard";
 import Nav from "./components/Nav";
 import Title from "./components/Title";
 import Wrapper from "./components/Wrapper";
 import './App.css';
 import cars from "./cars.json";
+import Container from "./Container";
+import Row from "./Row";
+import Column from "./Column";
 
 function shufflecars(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -24,7 +27,48 @@ class App extends Component {
   };
 }
 
-render () {
+handleClick = id => {
+  if (this.state.clicked.indexOf(id) === -1) {
+    this.handleIncrement();
+    this.setState({ clicked: this.state.clicked.concat(id) });
+  } else {
+    this.handleReset();
+  }
+};
+
+
+handleIncrement = () => {
+  const newScore = this.state.currentScore + 1;
+  this.setState({
+    currentScore: newScore,
+    rightWrong: ""
+  });
+  if (newScore >= this.state.topScore) {
+    this.setState({ topScore: newScore });
+  }
+  else if (newScore === 12) {
+    this.setState({ rightWrong: "You win!" });
+  }
+  this.handleShuffle();
+};
+
+
+handleReset = () => {
+  this.setState({
+    currentScore: 0,
+    topScore: this.state.topScore,
+    rightWrong: "Boo!",
+    clicked: []
+  });
+  this.handleShuffle();
+};
+
+handleShuffle = () => {
+  let shuffledcars = shufflecars(cars);
+  this.setState({ cars: shuffledcars });
+};
+
+render () 
   return (
     <Wrapper>
       <Nav
@@ -32,7 +76,7 @@ render () {
         score={this.state.currentScore}
         topScore={this.state.topScore}
         wrong={this.state.wrong}
-        />
+      />
 
         <Title>
           Click each car, but not more than once, or you're OUTTA THERE!
@@ -55,9 +99,9 @@ render () {
             ))}
           </Row>
         </Container>
-        </Wrapper>
+      </Wrapper>
     );
-  }
-}
+  
+
 
 export default App;
